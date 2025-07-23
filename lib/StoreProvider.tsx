33ccde74c956fe8e821e8ebb94729db39ce56dc6
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-22 20:53:52 -0700

    Fixup persistence logic

--- lib/StoreProvider.tsx
+++ lib/StoreProvider.tsx
@@ -5,19 +5,46 @@ import type { Persistor, PersistorOptions } from "redux-persist";
 import type { Store } from "@reduxjs/toolkit";
 import { makeStore } from "@/lib/store";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { createContext, use, useEffect, useRef } from "react";
+import {
+    createContext, use, useEffect, useRef,
+    useState,
+    useTransition
+} from "react";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 
-const neverPromise: Promise<void> = new Promise(() => {});
-
-const PersistorContext = createContext<Promise<void>>(neverPromise);
+const PersistorContext = createContext<Persistor | null>(null);
 PersistorContext.displayName = `PersistorContext`;
+const PersistorProvider = PersistorContext.Provider;
 
-// Integrate persistence with hydration and Suspense
-export const usePersisting = () => {
-    const ps = use(PersistorContext);
-    use(ps);
+
+const neverPromise = new Promise<void>(() => {});
+
+const PersistOnLoadContext = createContext<Promise<void>>(neverPromise);
+PersistOnLoadContext.displayName = `PersistOnLoadContext`;
+const PersistOnLoadProvider = PersistOnLoadContext.Provider;
+
+
+const PersistLoadedContext = createContext<boolean>(false);
+PersistLoadedContext.displayName = `PersistLoadedContext`;
+const PersistLoadedProvider = PersistLoadedContext.Provider;
+
+export const usePersistIsLoaded = () => use(PersistLoadedContext);
+
+export const usePersist = () => {
+    const persistor = use(PersistorContext);
+    useEffect(() => {
+        if (!persistor) {
+            return;
+        }
+        persistor.persist()
+    }, [persistor]);
+};
+
+// FIXME need to think through this
+export const usePersistLoaded = () => {
+    const onLoad = use(PersistOnLoadContext);
+    use(onLoad);
 };
 
 interface Props {
@@ -30,25 +57,54 @@ interface RefState {
     persistorPromise: Promise<void>;
 }
 
+const withResolvers = <T,>() => {
+    let resolve;
+    let reject;
+    const promise = new Promise<T>((res, rej) => {
+        resolve = res;
+        reject = rej;
+    });
+    return { promise, resolve: resolve!, reject: reject! };
+};
+
+const persistStoreAsync = (store: Store) => {
+    const { promise, resolve } = withResolvers<void>();
+    const persistor = persistStore(store, {
+        manualPersist: true
+    } as PersistorOptions, () => resolve());
+    return { persistor, promise };
+};
+
 export const StoreProvider = ({ children }: Props) => {
     const ref = useRef<RefState>(null);
+    const [loaded, setLoaded] = useState(false);
+    const [, startTransition] = useTransition();
+
     useEffect(() => setupListeners(ref.current!.store.dispatch), []);
-    useEffect(() => ref.current!.persistor.persist(), []);
+    useEffect(() => {
+        (async () => {
+            await ref.current!.persistorPromise;
+            startTransition(() => setLoaded(true));
+        })();
+    }, []);
 
     if (!ref.current) {
         const store = makeStore();
-        const box: { persistor?: Persistor } = { };
-        const persistorPromise = new Promise<void>(resolve => {
-            const config = { manualPersist: true } as PersistorOptions;
-            box.persistor = persistStore(store, config, () => resolve());
-        });
-        const persistor = box.persistor!;
-        ref.current = { store, persistor, persistorPromise };
-    }
+        const { persistor, promise: persistorPromise } = persistStoreAsync(store);
 
-    return <Provider store={ref.current.store!}>
-        <PersistorContext.Provider value={ref.current.persistorPromise}>
-                {children}
-        </PersistorContext.Provider>
+        ref.current = {
+            store,
+            persistor,
+            persistorPromise
+        };
+    }
+    return <Provider store={ref.current!.store}>
+            <PersistorProvider value={ref.current!.persistor}>
+                <PersistOnLoadProvider value={ref.current!.persistorPromise}>
+                    <PersistLoadedProvider value={loaded}>
+                        {children}
+                    </PersistLoadedProvider>
+                </PersistOnLoadProvider>
+            </PersistorProvider>
         </Provider>;
 };
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,23 +2,22 @@ import type { Selector } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, useStore } from "react-redux";
 
 import type { AppDispatch, AppStore, RootState } from "./store";
-import { usePersisting } from "./StoreProvider";
+import { usePersistIsLoaded, usePersist } from "./StoreProvider";
+
+export const useAppStore = useStore.withTypes<AppStore>();
 
 const useAppSelectorPrim = useSelector.withTypes<RootState>();
-const useAppStorePrim = useStore.withTypes<AppStore>();
+
 const useAppDispatchPrim = useDispatch.withTypes<AppDispatch>();
 
 export const useAppSelector = <T>(selector: Selector<RootState, T>) => {
-    usePersisting();
+    usePersist();
     return useAppSelectorPrim(selector);
 };
 
-export const useAppStore = () => {
-    usePersisting();
-    return useAppStorePrim();
-};
-
 export const useAppDispatch = () => {
-    usePersisting();
-    return useAppDispatchPrim();
+    usePersist();
+    const persisting = usePersistIsLoaded();
+    const dispatch = useAppDispatchPrim();
+    return persisting ? dispatch : () => {};
 };
