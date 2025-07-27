adc86e3d81878218de67e7fa63bcf7237809afda
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-27 13:20:19 -0700

    Adjust manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,8 +8,8 @@ const manifest: () => MetadataRoute.Manifest = () => ({
     description: 'A minimalist todo list app',
     start_url: process.env.PAGES_BASE_PATH,
     display: 'standalone',
-    background_color: '#600000',
-    theme_color: '#00C0FF',
+    background_color: '#1A0F00',
+    theme_color: '#FFEEDD',
     icons: [32, 192, 512].map(size => ({
         src: `${process.env.PAGES_BASE_PATH}/icon/icon-${size}x${size}.png`,
         sizes: `${size}x${size}`,
