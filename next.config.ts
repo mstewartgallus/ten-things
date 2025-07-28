8f86d5cdd6bd1012847c791d54241a6d81f4274a
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-27 21:52:09 -0700

    Add source maps

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: process.env.PAGES_BASE_PATH
+    basePath: process.env.PAGES_BASE_PATH,
+    productionBrowserSourceMaps: true
 };
 
 export default nextConfig;
