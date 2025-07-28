392aa69c3684508d77bd2074b5846f7dece8cee2
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-07-28 12:59:00 -0700

    Prototype more better heading field, look into modern form techniques

--- ui/fresh-edit/index.tsx
+++ ui/fresh-edit/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ReactNode, Ref, ChangeEvent, FormEvent, JSX, MouseEvent } from "react";
+import type { ReactNode, Ref, InputEvent, FormEvent, JSX, MouseEvent } from "react";
 import type { Entry } from '@/lib';
 import {
     useTransition, useCallback, useImperativeHandle, useMemo,
@@ -9,24 +9,37 @@ import {
 import { Button } from "../button";
 import { Time } from "../time";
 import { Icon } from "../icon";
-import { If } from "../If";
 
 import styles from "./FreshEdit.module.css";
 
 const iff = <T,>(cond: boolean, val: T) => cond ? val : undefined;
 
-interface EditHandle {
-    change(value: string): void;
+interface InputProps {
+    value?: string;
+    focusAction?: () => Promise<void>;
+    blurAction?: () => Promise<void>;
+    inputAction?: (value: string) => Promise<void>;
 }
 
-const useEdit = (ref: Ref<EditHandle>, initValue?: string) => {
-    const [value, setValue] = useState(initValue ?? '');
+const Input = ({ value, focusAction, blurAction, inputAction }: InputProps) => {
+    const ref = useRef<HTMLDivElement>(null);
 
-    useImperativeHandle(ref, () => ({
-        change: v => setValue(v)
-    }), []);
+    const onInput = useMemo(() => {
+        if (!inputAction) {
+            return;
+        }
+        return async (e: InputEvent<HTMLDivElement>) => {
+            let text = ref.current!.innerText;
+            await inputAction(text);
+        };
+    }, [inputAction]);
 
-    return value;
+    useEffect(() => {
+        ref.current!.innerText = value ?? '';
+    }, [value]);
+    return <div ref={ref} contentEditable={true}
+     aria-label="Title"  data-selected={true}
+    onFocus={focusAction} onBlur={blurAction} className={styles.input} onInput={onInput} />;
 };
 
 interface Props {
@@ -64,6 +77,7 @@ export const FreshEdit = ({
     const emptyValue = value === undefined;
 
     const [, startTransition] = useTransition();
+    const [editValue, setEditValue] = useState(value ?? '');
 
     const onClick = useMemo(() => {
         if (!toggleAction) {
@@ -75,84 +89,80 @@ export const FreshEdit = ({
         };
     }, [toggleAction]);
 
-    const ref = useRef<EditHandle>(null);
-    const editValue = useEdit(ref, value);
-
     const [focus, setFocus] = useState(false);
-
-    const onChangeInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-        const { currentTarget } = e;
-        if (!currentTarget) {
-            return;
-        }
-        e.preventDefault();
-        ref.current!.change(currentTarget.value);
-    }, []);
 
     const formAction = useMemo(() => {
         if (!changeAction) {
             return;
         }
-        return async (formData: FormData) => {
-            const title = (formData.get('title') ?? '') as string;
-            await changeAction(title);
+        return async () => {
+            await changeAction(editValue);
         };
-    }, [changeAction]);
+    }, [changeAction, editValue]);
 
-    const label = emptyValue ?
-        (selected ? 'Cancel Create' : 'Create Thing') :
-        (selected ? 'Cancel Edit' : 'Edit Thing');
+    const focusAction = useCallback(async() => startTransition(() => setFocus(true)), []);
+    const blurAction = useCallback(async() => startTransition(() => setFocus(false)), []);
 
-    const onFocus = useCallback(() => startTransition(() => setFocus(true)), []);
-    const onBlur = useCallback(() => startTransition(() => setFocus(false)), []);
+    useEffect(() => {
+        setEditValue(value ?? '');
+    }, [value]);
+    const inputAction = useCallback(async (value: string) => {
+        setEditValue(value ?? '');
+    }, []);
 
     const controlId = useId();
     const formId = useId();
     const id = useId();
 
+    const label = emptyValue ?
+        (selected ? 'Cancel Create' : 'Create Thing') :
+        (selected ? 'Cancel Edit' : 'Edit Thing');
+
+    const disclosureButton = <Button aria-label={label}
+        disabled={disabled || !onClick}
+        onClick={onClick}
+        aria-expanded={selected}
+        aria-controls={controlId}>
+            <Icon>{selected ? '🗙' : emptyValue ? '+' : '✎'}</Icon>
+        </Button>;
+    const disclosure = selected
+        ? <form id={formId} action={formAction}>
+            <Input value={value} focusAction={focusAction} blurAction={blurAction} inputAction={inputAction} />
+          </form>
+        : <div className={styles.input}>{value ?? '...'}</div>;
+
+    const completeButton = selected
+        ? <Button form={formId} disabled={disabled} aria-label="Edit Thing">
+              <Icon>D</Icon>
+          </Button>
+        : completeAction
+        && <form id={id} action={completeAction}>
+            <Button disabled={disabled || selected} aria-label="Complete Thing" value="complete">
+                <Icon>✔</Icon>
+            </Button>
+        </form>;
+
     return <div className={styles.fresh}>
-        {listItemMarker}
-        <div className={styles.freshItem}>
-        <div className={styles.editForm}>
-                    <Button aria-label={label}
-                                disabled={disabled || !onClick}
-                                onClick={onClick}
-                                aria-expanded={selected}
-                                aria-controls={controlId}>
-                             <Icon>{selected ? '🗙' : emptyValue ? '+' : '✎'}</Icon>
-                    </Button>
+            <div className={styles.freshItem}>
+                <div className={styles.editForm} data-focus={focus} data-selected={selected}>
+                    <div className={styles.inputWrapper} data-focus={focus} data-selected={selected}>
+                        <div className={styles.disclosureButton}>
+                        {listItemMarker}
+                        {disclosureButton}
+                        </div>
 
-                    <div id={controlId} className={styles.titleAndInput}>
-                        <If cond={!selected}>
-                            <div className={styles.title}>{value ?? '...'}</div>
-                        </If>
-                            <If cond={selected}>
-        <form id={formId} action={formAction} className={styles.inputForm}>
-                <input name="title" required
-            maxLength={144} value={editValue} onChange={onChangeInput}
-            onFocus={onFocus} onBlur={onBlur}
-            className={styles.input} data-focus={focus} />
-                                </form>
-                            </If>
+                        <div id={controlId}>
+                            {disclosure}
+                        </div>
                     </div>
+                    <div className={styles.floatRight}>
+                         {completeButton}
+                    </div>
+                </div>
 
-                <If cond={selected}>
-                    <Button form={formId} disabled={disabled}>
-                        Submit
-                    </Button>
-                </If>
-                <If cond={!!(!selected && completeAction)}>
-                    <form id={id} action={completeAction}>
-                        <Button disabled={disabled || selected}
-                                aria-label="Complete Thing" value="complete">
-                            <Icon>✔</Icon>
-                        </Button>
-                    </form>
-                </If>
-           </div>
-            <div className={styles.metadata}>
-                {children}
+                <div className={styles.metadata}>
+                    {children}
+                </div>
             </div>
-          </div>
         </div>;
 };
