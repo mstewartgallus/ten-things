a3a929762482fc8cbdbf6a2c4de6bcc708ab48f8
Molly Stewart-Gallus <mollystewartgallus@gmail.com>
2025-08-05 20:17:58 -0700

    Abandon shadow-dom approach to input

--- ui/fresh-edit/index.tsx
+++ ui/fresh-edit/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ReactNode, Ref, InputEvent, FormEvent, JSX, MouseEvent } from "react";
+import type { ReactNode, Ref, ClipboardEvent, KeyboardEvent, InputEvent, FormEvent, JSX, MouseEvent } from "react";
 import type { Entry } from '@/lib';
 import {
     useTransition, useCallback, useImperativeHandle, useMemo,
@@ -15,37 +15,144 @@ import styles from "./FreshEdit.module.css";
 
 const iff = <T,>(cond: boolean, val: T) => cond ? val : undefined;
 
-interface Props {
+const getCaret = () => {
+    const selection = window.getSelection();
+    if (!selection) {
+        return [0, 0];
+    }
+    const range = selection.getRangeAt(0);
+    return [range.startOffset, range.endOffset];
+};
+
+interface LayoutProps {
     children?: ReactNode;
+    title?: ReactNode;
+    titleButton?: ReactNode;
+    completeButton: ReactNode;
 
+    controlId: string;
+
+    selected?: boolean;
+    focus?: boolean;
+}
+
+export const FreshLayout = ({
+    selected = false,
+    focus = false,
+    children,
+    title,
+    titleButton,
+    controlId,
+    completeButton
+}: LayoutProps) =>
+    <div className={styles.item}>
+        <div className={styles.freshItem} data-selected={selected}>
+            <div className={styles.inputWrapper} data-selected={selected} data-focus={focus}>
+                <div className={styles.disclosureButton}>
+                    {titleButton}
+                </div>
+
+                <div id={controlId} className={styles.title} data-selected={selected}>
+                    {title}
+                </div>
+             </div>
+        </div>
+
+        <div className={styles.widgets}>
+            {completeButton}
+        </div>
+
+        <div className={styles.metadata}>
+            {children}
+        </div>
+    </div>;
+
+interface ReadingProps {
+    controlId: string;
+    children?: ReactNode;
     disabled: boolean;
-
     entry?: Entry;
-
-    selected: boolean;
-
     changeAction?: (value: string) => Promise<void>;
     toggleAction?: () => Promise<void>;
-
     completeAction?: () => Promise<void>;
 }
 
-export const FreshEdit = ({
+export const FreshReading = ({
+    controlId,
+    entry,
+    children,
+
+    disabled,
+
+    changeAction,
+    toggleAction,
+    completeAction
+}: ReadingProps) => {
+    const emptyValue = !entry;
+    const value = entry && entry.value
+
+    const onClick = useMemo(() => {
+        if (!toggleAction) {
+            return;
+        }
+        return async (e: MouseEvent<HTMLButtonElement>) => {
+            e.preventDefault();
+            await toggleAction();
+        };
+    }, [toggleAction]);
+
+    const id = useId();
+    return <FreshLayout
+        controlId={controlId}
+            titleButton={
+                <Button aria-label={emptyValue ? 'Create Thing' : 'Edit Thing'}
+                    disabled={disabled || !onClick}
+                    onClick={onClick}
+                    aria-expanded={false}
+                    aria-controls={controlId}>
+                    <Icon>{emptyValue ? '+' : '✎'}</Icon>
+                </Button>
+            }
+            title={value ?? '...'}
+            completeButton={
+                completeAction
+                    && <form id={id} action={completeAction}>
+                        <Button disabled={disabled} aria-label="Complete Thing" value="complete">
+                            <Icon>✔</Icon>
+                        </Button>
+                    </form>
+            }>
+            {children}
+    </FreshLayout>;
+};
+
+interface EditingProps {
+    controlId: string;
+    children?: ReactNode;
+    disabled: boolean;
+    entry?: Entry;
+    changeAction?: (value: string) => Promise<void>;
+    toggleAction?: () => Promise<void>;
+    completeAction?: () => Promise<void>;
+    maxLength?: number;
+}
+
+export const FreshEditing = ({
+    controlId,
     children,
 
     disabled,
 
     entry,
-    selected,
 
     changeAction,
     toggleAction,
 
-    completeAction
-}: Props) => {
-    const value = entry && entry.value;
-    const created = entry && entry.created;
-    const emptyValue = value === undefined;
+    completeAction,
+    maxLength
+}: EditingProps) => {
+    const emptyValue = !entry;
+    const initialValue = (entry && entry.value) ?? '';
 
     const [, startTransition] = useTransition();
 
@@ -60,78 +167,202 @@ export const FreshEdit = ({
     }, [toggleAction]);
 
     const [focus, setFocus] = useState(false);
-    if (!selected && focus) {
-        setFocus(false);
-    }
+
+    const onFocus = useCallback(async () => startTransition(() => setFocus(true)), []);
+    const onBlur = useCallback(async () => startTransition(() => setFocus(false)), []);
+
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
+
+    const [invalid, setInvalid] = useState(false);
+    const [value, setValue] = useState(initialValue);
+    const [selection, setSelection] = useState<number | null>(null);
 
     const formAction = useMemo(() => {
         if (!changeAction) {
             return;
         }
-        return async (d: FormData) => {
-            // FIXME verify
-            const title = d.get('title') as string;
-            await changeAction(title);
+        return async () => {
+            await changeAction(value);
         };
-    }, [changeAction]);
+    }, [changeAction, value]);
 
-    const onFocus = useCallback(() => startTransition(() => setFocus(true)), []);
-    const onBlur = useCallback(() => startTransition(() => setFocus(false)), []);
+    const edit = useCallback(async (value: string, selection: number) => {
+        setValue(value);
+        setSelection(selection);
+    }, []);
 
-    const controlId = useId();
-    const formId = useId();
+    const backspaceAction = useCallback(async () => {
+        let [selectionStart, selectionEnd] = getCaret();
+        if (selectionStart === selectionEnd) {
+            selectionStart -= 1;
+        }
+        if (selectionStart < 0) {
+            selectionStart = 0;
+        }
+        const newValue = value.substring(0, selectionStart) + value.substring(selectionEnd);
+        await edit(newValue, selectionStart);
+    }, [value, edit]);
+
+    const deleteAction = useCallback(async () => {
+        let [selectionStart, selectionEnd] = getCaret();
+        if (selectionStart === selectionEnd) {
+            selectionEnd += 1;
+        }
+        if (selectionEnd >= value.length) {
+            selectionEnd = value.length;
+        }
+        const newValue = value.substring(0, selectionStart) + value.substring(selectionEnd);
+        await edit(newValue, selectionStart);
+    }, [value, edit]);
+
+    const inputAction = useCallback(async (data: string) => {
+        data = data.replace('\n', ' ');
+
+        const [selectionStart, selectionEnd] = getCaret();
+        const newValue =
+            value.substring(0, selectionStart) +
+            data +
+            value.substring(selectionEnd);
+        await edit(newValue, selectionStart + data.length);
+    }, [value, edit]);
+
+    const onBeforeInput = useCallback((e: InputEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        inputAction(e.data);
+    }, [inputAction]);
+
+    const onPaste = useCallback((e: ClipboardEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        inputAction(e.clipboardData.getData('text'));
+    }, [inputAction]);
+
+    // FIXME handle deletion better, also cut
+    const onKeyDown = useCallback((e: KeyboardEvent<HTMLDivElement>) => {
+        const { key } = e;
+        if (key !== 'Enter' && key !== 'Backspace' && key !== 'Delete') {
+            return;
+        }
+        e.preventDefault();
+
+        switch (key) {
+            case 'Enter':
+                buttonRef.current!.click();
+                break;
+
+            case 'Backspace':
+                if (backspaceAction) {
+                    backspaceAction();
+                }
+                break;
+            case 'Delete':
+                if (deleteAction) {
+                    deleteAction();
+                }
+                break;
+        }
+    }, [invalid, backspaceAction, deleteAction]);
+
+    useEffect(() => {
+        const messages = [];
+        const valueMissing = value.length === 0;
+        const tooLong = !!maxLength && value.length >= maxLength;
+
+        if (valueMissing) {
+            // FIXME put cleaner error messages
+            messages.push('Thing title is required');
+        }
+        if (tooLong) {
+            // FIXME put cleaner error messages
+            messages.push('Thing title is too long');
+        }
+
+        setInvalid(messages.length > 0);
+
+        const elem = buttonRef.current;
+        if (!elem) {
+            return;
+        }
+        elem.setCustomValidity(messages.join('\n'));
+    }, [maxLength, value]);
+
+    // FIXME do this on focus...
+    useEffect(() => {
+        if (!selection) {
+            return;
+        }
+        const elem = ref.current!;
+        const sel = window.getSelection();
+        if (!sel) {
+            throw Error("huh?");
+        }
+        sel.collapse(elem.firstChild, selection);
+    }, [selection]);
+
     const id = useId();
 
-    const label = emptyValue ?
-        (selected ? 'Cancel Create' : 'Create Thing') :
-        (selected ? 'Cancel Edit' : 'Edit Thing');
-
-    const disclosureButton = <Button aria-label={label}
-        disabled={disabled || !onClick}
-        onClick={onClick}
-        aria-expanded={selected}
-        aria-controls={controlId}>
-            <Icon>{selected ? '🗙' : emptyValue ? '+' : '✎'}</Icon>
-        </Button>;
-    const disclosure = selected
-        ? <form id={formId} action={formAction}>
-           <Input name="title" value={value} maxLength={300} required
-               onFocus={onFocus} onBlur={onBlur}
-               aria-label="title"
-            />
-          </form>
-        : value ?? '...';
-
-    const completeButton = selected
-        ? <Button form={formId} disabled={disabled} aria-label="Edit Thing">
-              <Icon>D</Icon>
-          </Button>
-        : completeAction
-        && <form id={id} action={completeAction}>
-            <Button disabled={disabled || selected} aria-label="Complete Thing" value="complete">
-                <Icon>✔</Icon>
-            </Button>
-        </form>;
-
-    return <div className={styles.item}>
-            <div className={styles.freshItem} data-focus={focus}>
-              <div className={styles.inputWrapper} data-selected={selected} data-focus={focus}>
-                    <div className={styles.disclosureButton}>
-                        {disclosureButton}
-                    </div>
-
-                    <div id={controlId} className={styles.title}>
-                        {disclosure}
+    return <FreshLayout
+        selected={true}
+        focus={focus}
+        controlId={controlId}
+        titleButton={
+            <Button aria-label={emptyValue ? 'Cancel Create Thing' : 'Cancel Edit Thing'}
+            disabled={disabled || !onClick}
+            onClick={onClick}
+            aria-expanded={true}
+            aria-controls={controlId}>
+                <Icon>🗙</Icon>
+                </Button>
+        }
+        title={
+            <div className={styles.inputWrapper}>
+                    <div ref={ref}
+                        className={styles.input}
+                        inputMode="text"
+                        role="textbox"
+                        aria-disabled={disabled}
+                        aria-required={true}
+                        aria-invalid={invalid}
+                        contentEditable={!disabled} suppressContentEditableWarning={true}
+                        onBeforeInput={onBeforeInput}
+                        onPaste={onPaste}
+                        onKeyDown={onKeyDown}
+                        onFocus={onFocus}
+                        onBlur={onBlur}
+                        >
+                        {value}
                     </div>
                 </div>
-            </div>
+        }
+        completeButton={
+            <form id={id} action={formAction}>
+                    <Button ref={buttonRef} disabled={disabled} aria-label="Edit Thing">
+                        <Icon>D</Icon>
+                    </Button>
+                </form>
+        }
+        >
+        {children}
+    </FreshLayout>;
+};
 
-            <div className={styles.widgets}>
-                {completeButton}
-            </div>
+interface Props {
+    selected: boolean;
 
-            <div className={styles.metadata}>
-                {children}
-            </div>
-        </div>;
+    children?: ReactNode;
+    disabled: boolean;
+    entry?: Entry;
+    changeAction?: (value: string) => Promise<void>;
+    toggleAction?: () => Promise<void>;
+    completeAction?: () => Promise<void>;
+}
+
+export const FreshEdit = (props: Props) => {
+    const { selected, ...leftover } = props;
+    const controlId = useId();
+
+    if (!selected) {
+        return <FreshReading {...leftover} controlId={controlId} />;
+    }
+    return <FreshEditing {...leftover} controlId={controlId} maxLength={300} />;
 };
